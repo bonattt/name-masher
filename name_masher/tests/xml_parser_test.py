986d9c5c866ef4a79d3a35d886551bb248edbcd4
bonattt <bonattt@rose-hulman.edu>
2017-01-28 21:21:03 -0500

    wrote empty test suite for MasherXmlParser
    wrote several empty test cases for the xmlParser. The test cases are not
    complete yet.

--- name_masher/tests/xml_parser_test.py
+++ name_masher/tests/xml_parser_test.py
@@ -25,6 +25,45 @@ class ConfigTest(unittest.TestCase):
         self.parser.parse_schema(xml)
         self.assertEqual(" ", self.parser.separator)
 
+    def test_constant_rule(self):
+        xml = '<constant> hello </constant>'
+        generators = self.parser.parse_schema(xml)
+        self.assertEquals(1, len(generators))
+
+    def test_list_rule(self):
+        xml = ''
+        generators = self.parser.parse_schema(xml)
+        self.assertEquals(1, len(generators))
+
+    def test_list_file_rule(self):
+        xml = ''
+        generators = self.parser.parse_schema(xml)
+        self.assertEquals(1, len(generators))
+
+    def test_random_rule(self):
+        xml = ''
+        generators = self.parser.parse_schema(xml)
+        self.assertEquals(1, len(generators))
+
+    def test_phrase_rule(self):
+        xml = ''
+        generators = self.parser.parse_schema(xml)
+        self.assertEquals(1, len(generators))
+
+    def test_rule_executes_on_all_tags(self):
+        xml = ''
+        self.parser.add_rule()
+        generators = self.parser.parse_schema(xml)
+
+
+class TagTallyRule:
+
+    def __init__(self):
+        self.tally = []
+
+    def apply_rule(self, element, layer):
+        self.tally.append(element.tag)
+
 
 if __name__ == "__main__":
     print("xml_parser_test.py")
